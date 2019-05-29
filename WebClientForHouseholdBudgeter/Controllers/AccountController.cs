956bf7de2fc9bf0ea3a76f025b27245b930ef60e
youngsoo shin <sys920@gmail.com>
2019-05-29 12:44:08 -0500

    12pm

---
+++ WebClientForHouseholdBudgeter/Controllers/AccountController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebClientForHouseholdBudgeter.Models.ViewModels.Account;
+
+namespace WebClientForHouseholdBudgeter.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Register()
+        {
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Register(RegisterUserViewModel formData)
+        {
+            
+
+            return View();
+        }
+    }
+}
---
+++ WebClientForHouseholdBudgeter/Models/ViewModels/Account/RegisterUserViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebClientForHouseholdBudgeter.Models.ViewModels.Account
+{
+    public class RegisterUserViewModel
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string ConfirmPassword { get; set; }
+    }
+}
