c64375310e6c887b2c3e488a262d404769bd0552
youngsoo shin <sys920@gmail.com>
2019-05-29 23:37:13 -0500

    Add user register and login UI

--- WebClientForHouseholdBudgeter/Controllers/AccountController.cs
+++ WebClientForHouseholdBudgeter/Controllers/AccountController.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Web;
 using System.Web.Mvc;
 using WebClientForHouseholdBudgeter.Models.ViewModels.Account;
@@ -9,11 +11,6 @@ namespace WebClientForHouseholdBudgeter.Controllers
 {
     public class AccountController : Controller
     {
-        // GET: Account
-        public ActionResult Login()
-        {
-            return View();
-        }
 
         [HttpGet]
         public ActionResult Register()
@@ -25,9 +22,73 @@ namespace WebClientForHouseholdBudgeter.Controllers
         [HttpPost]
         public ActionResult Register(RegisterUserViewModel formData)
         {
-            
+            if(!ModelState.IsValid)
+            {
+                return View();
+            }
 
+            var url = $"http://localhost:55336/api/Account/Register";
+            var httpClient = new HttpClient();
+            var parameters = new List<KeyValuePair<string, string>>();
+            parameters.Add(new KeyValuePair<string, string>("Email", formData.Email));
+            parameters.Add(new KeyValuePair<string, string>("Password", formData.Password));
+            parameters.Add(new KeyValuePair<string, string>("ConfirmPassword", formData.ConfirmPassword));
+
+            var enCodeParameters = new FormUrlEncodedContent(parameters);
+
+            var response = httpClient.PostAsync(url, enCodeParameters).Result;
+            if(response.StatusCode == System.Net.HttpStatusCode.OK)              
+            {
+                var data = response.Content.ReadAsStringAsync().Result;
+
+                return RedirectToAction("Login", "Account");
+            }
             return View();
         }
+
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(LoginViewModel formData)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+
+            var url = $"http://localhost:55336/Token";
+            var grantType = "password";
+
+            var httpClient = new HttpClient();
+            var parameters = new List<KeyValuePair<string, string>>();
+            parameters.Add(new KeyValuePair<string, string>("username", formData.Email));
+            parameters.Add(new KeyValuePair<string, string>("Password", formData.Password));
+            parameters.Add(new KeyValuePair<string, string>("grant_type", grantType));
+
+
+            var enCodeParameters = new FormUrlEncodedContent(parameters);
+
+            var response = httpClient.PostAsync(url, enCodeParameters).Result;
+
+            if (response.StatusCode == System.Net.HttpStatusCode.OK)
+            {
+                var data = response.Content.ReadAsStringAsync().Result;
+
+                var result = JsonConvert.DeserializeObject<LoginToken>(data);
+
+                var cookie = new HttpCookie("BBCookie",result.AccessToken);
+                Response.Cookies.Add(cookie);
+
+                return RedirectToAction("Index", "Home");
+            }
+            return View();
+        }
+
+
+
     }
 }
--- WebClientForHouseholdBudgeter/Models/ViewModels/Account/RegisterUserViewModel.cs
+++ WebClientForHouseholdBudgeter/Models/ViewModels/Account/RegisterUserViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,8 +8,20 @@ namespace WebClientForHouseholdBudgeter.Models.ViewModels.Account
 {
     public class RegisterUserViewModel
     {
+        [Required]
+        [Display(Name = "Email")]
         public string Email { get; set; }
+
+        [Required]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
         public string Password { get; set; }
+
+      
+        [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm password")]
         public string ConfirmPassword { get; set; }
     }
 }
